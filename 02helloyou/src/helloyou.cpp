ef358b27acd57f25bfb0bf79d1beba1c6599979d
Shane Reynolds <shane.k.reynolds@gmail.com>
2017-03-14 10:21:42 +1100

    Add a basic script which asks the user to input their name and greets them afterwards.

---
+++ 02helloyou/src/helloyou.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	string name;
+	cout << "Enter your name: \n";
+	cin >> name;
+	cout << "Hello " << name << "!\n" << endl;
+	return 0;
+}
