4bdd0214cb788a6b8c4506f259a7975f51eaacab
Shane Reynolds <shane.k.reynolds@gmail.com>
2017-03-14 16:11:14 +1100

    Added comments to the files, which were previously uncommented. Shame.

--- 02helloyou/src/helloyou.cpp
+++ 02helloyou/src/helloyou.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+/*Write a program that asks the user for her name and
+ * greets her with her name.
+ *
+ * Author: Shane Reynolds
+ *
+ * */
+
 int main(void){
 	string name;
 	cout << "Enter your name: \n";
--- 03hellobob/src/hellobob.cpp
+++ 03hellobob/src/hellobob.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+/*Modify the previous program such that only
+ * the users Alice and Bob are greeted with their
+ * names.
+ *
+ * Author: Shane Reynolds
+ *
+ * */
+
 int main(void){
 	string name;
 	cout << "Enter your name:\n";
