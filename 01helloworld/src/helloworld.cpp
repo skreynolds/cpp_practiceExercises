06fb4679fccd76b7652e6912233a146532037f3e
Shane Reynolds <shane.k.reynolds@gmail.com>
2017-03-14 15:50:03 +1100

    Add comments to the code

--- 01helloworld/src/helloworld.cpp
+++ 01helloworld/src/helloworld.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+/*Write a program that prints ‘Hello World’ to the screen.
+ *
+ * Author: Shane Reynolds
+ *
+ * */
+
 int main(void){
 	cout << "Hello world!" << endl;
 	return 0;
