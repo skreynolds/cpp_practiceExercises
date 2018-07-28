52d406ef30cdba624e43a6f5ea90d2aea1a258e5
skreynolds <shane.k.reynolds@gmail.com>
2018-07-28 12:26:49 +0930

    exercise: Added an exercise to find even numbers from an array
    Function took an array and printed all the even numbers stored in the array. There are some additional file updates for matrix exercises which were part of a previous exercise using the eigen3 library for cpp

---
+++ 18findEven/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "main.hpp"
+
+int my_array[11] = {0,1,2,3,4,5,6,7,8,9,10};
+
+int main(void){
+	FindEven(my_array);
+	return 0;
+}
---
+++ 18findEven/main.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void FindEven(int array[]){
+	for (int i=0; i<=10; i++){
+			if(array[i] % 2 == 0){
+				std::cout << array[i] << std::endl;
+			}
+	}
+}
+
+
+
